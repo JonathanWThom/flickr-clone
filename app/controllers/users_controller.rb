94c952ca9c440bdb5335186fcf0e735040e73a06
Cody Brubaker and Jonathan Thom <cody+jonathan@>
2017-02-15 15:50:56 -0800

    add admin privs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
-  
+  before_filter :authenticate_user!
+  before_filter do
+    redirect_to :new_user_session_path unless current_user && current_user.admin?
+  end
+
+  def index
+    @users = User.all
+
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
 end
