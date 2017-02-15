3def524139fdcfbf0a656fd107552661968fed08
Cody Brubaker and Jonathan Thom <cody+jonathan@>
2017-02-15 11:22:58 -0800

    Ready to push

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
+  before_filter :authenticate_user!, except: [:index]
   def index
     @user = User.find(params[:user_id])
     @photos = Photo.where(owner_id: @user.id)
