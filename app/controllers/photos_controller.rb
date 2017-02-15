30fdb92374307bd66409757d873f0c4489e3f1d5
Cody Brubaker and Jonathan Thom <cody+jonathan@>
2017-02-15 10:36:47 -0800

    upload photos with paperclip

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,23 @@
 class PhotosController < ApplicationController
   def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(image: photo_params[:image], owner_id: current_user.id)
+    if @photo.save
+      redirect_to photos_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-   root :to => 'photos#index'
+  root :to => 'photos#index'
+
+  resources :users do
+    resources :photos
+  end
+
+  resources :users do
+    resources :tags
+  end
+
+  resources :photos do
+
+  end
 end
