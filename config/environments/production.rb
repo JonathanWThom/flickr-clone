6b74a6aa0cbaa07af5cb1db6c6e5d9329f44db65
Cody Brubaker and Jonathan Thom <cody+jonathan@>
2017-02-16 14:30:18 -0800

    Add mailgun settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,16 @@ Rails.application.configure do
     s3_region: ENV.fetch('AWS_REGION'),
   }
 }
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'evening-peak-13128.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
